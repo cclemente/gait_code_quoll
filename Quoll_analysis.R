ae2798b8ff830ff95d4aa65243e1db715a27e76c
Chris Clemente <cclement@usc.edu.au>
2016-11-28 10:54:52 +1000

    Add files via upload

---
+++ Quoll_analysis.R
@@ -0,0 +1,40 @@
+
+
+setwd("G:/Quoll gait paper")
+data5<-read.csv("quoll_surf_num_cali.csv")
+
+cols<-c("red","blue","green","pink")
+boxplot(SpC~X2, data=data5)
+boxplot(SpC~Gait+X2,col=cols, data=data5)
+summary(fit<-aov(SpC~Gait+X2, data=data5))
+TukeyHSD(fit)
+
+library(lattice)
+xyplot(Plag~DF,data=data5,groups=X2)
+
+boxplot(DF~X2, data=data5)
+#duty factor is highest on the pole - reflects slow speed
+
+boxplot(Duration~X2, data=data5)
+#but duration is also low on the pole??
+
+xyplot(SpC~DF,data5,group=X2, auto.key = T)
+
+#swing time is important??? 
+
+xyplot(Plag~DF,data5,group=X2, auto.key = T)
+xyplot(Flag~DF,data5,group=X2, auto.key = T)#let this be +vs / -ve??
+xyplot(Hlag~DF,data5,group=X2, auto.key = T)# which is left vs right lead?? 
+
+
+#stride distance?
+xyplot(sdf~DF,data5,group=X2, auto.key = T)
+
+boxplot(SpC~Gait, data5)
+summary(fit<-aov(SpC~Gait+Error(QuollRun), data=data5))# significant effect - but not convinving! 
+TukeyHSD(fit)
+
+
+boxplot(DF~Gait, data5)
+
+
