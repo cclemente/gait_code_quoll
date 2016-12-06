2c29ca80da66ee03a8e04e92666050a84db696e6
cclemente <cclement@usc.edu.au>
2016-12-06 15:08:22 +1000

    commited

---
+++ Quoll_combine.R
@@ -0,0 +1,42 @@
+
+
+setwd("D:/project files/quoll paper/Analyse Quoll Gait")#clemente computer
+#setwd("H:/quoll raw data/Quoll/Analyse Quoll Gait")#josh usb
+#setwd("D:/Quoll/Analyse Quoll Gait")#Josh Laptop
+
+filenames <- (Sys.glob("*xypts.csv"))
+
+cvalues2= matrix(nrow = 0, ncol = 13)
+colnames(cvalues2)<-c('QuollRun', 'Cycle','Duration','Plag','Flag','Hlag', 'DF','DFH','DFF','Speed','Gait','sdh','sdf')
+
+strides_out = matrix(nrow = 0, ncol = 9)
+colnames(strides_out)<-c("filename","Stride","Foot","FF" ,"ES","SubFF","DF","ES-FF","SubFF-FF")
+
+#ii=14
+#for (ii in 1:10){
+for (ii in 1:length(filenames)){
+
+filename<-filenames[ii]
+
+strides<-DF(filename) 
+stff_out<-stff(filename,strides)
+file_name<-rep_len(filename, nrow(strides))
+#add line of code which finds surface magically....
+#surface=rep_len("flat",nrow(strides))
+
+strides<-cbind(file_name,strides)
+
+strides_out = rbind(strides_out,strides)
+
+cvalues2 = rbind(cvalues2, stff_out)
+
+}
+
+#setwd("D:/project files/quoll paper")#clemente computer
+setwd("H:/quoll raw data/Quoll/quoll code")#josh usb
+#setwd("D:/Quoll/quoll code")#josh laptop
+write.csv(cvalues2,"Quoll_strides_averaged9.csv")
+write.csv(strides_out,"Quoll_steps_all9.csv")
+
+
+
---
+++ surfacefinder.R
@@ -0,0 +1,46 @@
+
+
+
+QFSD<-matrix(nrow = length(data1$QuollRun), ncol = 3)
+QFSD[,1]<-as.character(data1$QuollRun)
+xx=1
+for (xx in 1:length(QFSD[,1])) {
+  if (length(grep('angle', QFSD[xx,1], value = TRUE))==1){
+    QFSD[xx,2]<-"angle"
+  }
+  if (length(grep('flat', QFSD[xx,1], value = TRUE))==1){
+   QFSD[xx,2]<-"flat"
+  }
+  if (length(grep('pole', QFSD[xx,1], value = TRUE))==1){
+   QFSD[xx,2]<-"pole"
+  }
+}
+data3<-cbind(data1,QFSD)
+setwd("G:/Quoll gait paper")
+write.csv(data3,"quoll_surf.csv")
+
+
+setwd("G:/Quoll gait paper")
+data1<-read.csv("Quoll_strides_averaged9.csv")
+data3<-read.csv("Quoll_strides_averaged9.csv")
+data2<-read.csv("name_key_quoll.csv")
+caliQ<-read.csv("cali_quoll.csv")
+
+surf2 = data.frame(name=character(),num=integer())
+#caliQ[which(caliQ[,1]==data2$calnum[ind[1]]),2]
+
+ii=1
+for (ii in 1:nrow(data3)){
+  ind<-which(grepl(data3$QuollRun[ii],data2$fname)==TRUE)
+  temp<-data.frame(name=data3$QuollRun[ii], num=caliQ[which(caliQ[,1]==data2$calnum[ind[1]]),2])
+  surf2<-rbind(surf2,temp)
+}
+
+head(surf2)
+
+data5<-cbind(data3,surf2)
+
+setwd("G:/Quoll gait paper")
+write.csv(data5,"quoll_surf_num_cali.csv")
+
+
